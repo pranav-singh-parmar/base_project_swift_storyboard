971714634d62a94c29108952ea46b61b498cfe42
pranav-singh-parmar <71075740+pranav-singh-parmar@users.noreply.github.com>
2022-08-18 20:34:57 +0530

    added project files

---
+++ base_project/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  base_project
+//
+//  Created by Pranav Singh on 18/08/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
